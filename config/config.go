44047f23d9ec82f62f19957cb74c32022eb2460d
Aditya Gupta <adityagupta2087@gmail.com>
2025-06-18 18:14:12 +0530

    Implemented more config functions

--- config/config.go
+++ config/config.go
@@ -11,6 +11,7 @@ func InitConfig[T any](configModel T, appName, filename string) error {
 	if err != nil {
 		return err
 	}
+
 	configFilePath := filepath.Join(cacheDir, appName, filename)
 	if !isFileExists(configFilePath) {
 		if err := os.MkdirAll(filepath.Join(cacheDir, appName), 0755); err != nil {
@@ -37,6 +38,80 @@ func InitConfig[T any](configModel T, appName, filename string) error {
 
 	return nil
 
+}
+
+func WriteConfig[T any](configModel T, appName, filename string) error {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return err
+	}
+
+	configFilePath := filepath.Join(cacheDir, appName, filename)
+
+	if isFileExists(configFilePath) {
+		jsonData, err := json.MarshalIndent(configModel, "", "  ")
+		if err != nil {
+			return err
+		}
+
+		if err := os.WriteFile(configFilePath, jsonData, 0644); err != nil {
+			return err
+		}
+	} else {
+		return os.ErrNotExist
+	}
+
+	return nil
+}
+
+func ModifyConfig[T any](configModel *T, appName, filename string) error {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return err
+	}
+
+	configFilePath := filepath.Join(cacheDir, appName, filename)
+
+	if !isFileExists(configFilePath) {
+		return os.ErrNotExist
+	}
+
+	data, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(data, configModel); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetConfig[T any](appName, filename string) (T, error) {
+	var configModel T
+
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return configModel, err
+	}
+
+	configFilePath := filepath.Join(cacheDir, appName, filename)
+
+	if !isFileExists(configFilePath) {
+		return configModel, os.ErrNotExist
+	}
+
+	data, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return configModel, err
+	}
+
+	if err := json.Unmarshal(data, &configModel); err != nil {
+		return configModel, err
+	}
+
+	return configModel, nil
 }
 
 func isFileExists(path string) bool {
--- main.go
+++ main.go
@@ -8,9 +8,16 @@ type User struct {
 	Email string `json:"email"`
 }
 
+//TODO: Documentation & Testing
 func main() {
 	var user User
 	if err := config.InitConfig(user, "aasdigo", "config.json"); err != nil {
+		panic(err)
+	}
+
+	user.Email = "aditya@gmail.com"
+
+	if err := config.WriteConfig(user, "aasdigo", "config.json"); err != nil {
 		panic(err)
 	}
 }
